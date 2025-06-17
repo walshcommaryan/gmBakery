76759f941a09c1cc26783bc51e72f1e0279d8f30
walshcommaryan <rywalsh@uci.edu>
2025-06-17 12:16:57 -0400

    feat: re arrange products

--- src/data/ProductsHelper.tsx
+++ src/data/ProductsHelper.tsx
@@ -1,14 +1,14 @@
 export const imageMap: { [key: string]: string[] } = {
   "Brioche Caramel Pecan nut": ["/assets/images/product_images/gm_10.png"],
   "Brioche Chocolate Chip": ["/assets/images/product_images/gm_6.png"],
-  "Canelés": ["/assets/images/product_images/gm_3a.png", "/assets/images/product_images/gm_3b.png"],
-  "Fondant Chocolat": ["/assets/images/product_images/gm_8a.png", "/assets/images/product_images/gm_8b.png"],
+  "Canelés": ["/assets/images/product_images/gm_3a.png", "/assets/images/product_images/gm_3b.png", "/assets/images/product_images/gm_3c.png"],
+  "Fondant Chocolat": ["/assets/images/product_images/gm_8a.png", "/assets/images/product_images/gm_8b.png", "/assets/images/product_images/gm_8c.png"],
   "Kouign Amann": ["/assets/images/product_images/gm_5.png"],
   "Lunette Apricot jam": ["/assets/images/product_images/gm_11.png"],
   "Lunette Strawberry Jam": ["/assets/images/product_images/gm_4.png"],
   "Macarons":[ "/assets/images/product_images/gm_2.png"],
   "Madeleines Chocolate Chip": ["/assets/images/product_images/gm_12a.png", "/assets/images/product_images/gm_12b.png"],
   "Madeleines Vanilla": ["/assets/images/product_images/gm_1a.png", "/assets/images/product_images/gm_1b.png"],
-  "Quiche Lorraine": ["/assets/images/product_images/gm_9.png"],
-  "Tarte au citron": ["/assets/images/product_images/gm_7.png"],
+  "Quiche Lorraine": ["/assets/images/product_images/gm_9a.png", "/assets/images/product_images/gm_9b.png"],
+  "Tarte au citron meringué": ["/assets/images/product_images/gm_7.png"],
 };
