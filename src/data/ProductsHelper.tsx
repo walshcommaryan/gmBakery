89621592ddfbac419b9607d68fddc3c25f4a50a8
walshcommaryan <rywalsh@uci.edu>
2025-06-18 10:00:03 -0400

    fix: madeleines map image

--- src/data/ProductsHelper.tsx
+++ src/data/ProductsHelper.tsx
@@ -8,7 +8,7 @@ export const imageMap: { [key: string]: string[] } = {
   "Lunette Strawberry Jam": ["/assets/images/product_images/gm_4.png"],
   "Macarons":[ "/assets/images/product_images/gm_2.png"],
   "Madeleines Chocolate Chip": ["/assets/images/product_images/gm_12a.png", "/assets/images/product_images/gm_12b.png"],
-  "Madeleines Vanilla": ["/assets/images/product_images/gm_1a.png", "/assets/images/product_images/gm_1b.png"],
+  "Madeleines Vanilla": ["/assets/images/product_images/gm_1.png"],
   "Quiche Lorraine": ["/assets/images/product_images/gm_9a.png", "/assets/images/product_images/gm_9b.png"],
   "Tarte au citron meringué": ["/assets/images/product_images/gm_7.png"],
 };
